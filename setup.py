02b6e69110631eb706715e30919f3a644bbdb085
Keith Philpott <keith@digitalbutter.com>
2017-03-17 00:08:04 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='simple_html',
+      version='0.0.1',
+      description='Simple HTML generator',
+      author='Keith Philpott',
+      url='https://gitlab.com/keithasaurus/simple_html',
+      packages=['simple_html'])
