bfc60147f671725575a718d3cced25665ca3d4cc
Keith Philpott <keith@digitalbutter.com>
2017-03-23 16:51:23 -0700

    update setup.py version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='simple_html',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='Simple HTML generator',
     author='Keith Philpott',
