507bafb698a7622d6fba786f1fae88f00d5e0365
Keith Philpott <keith.philpott@graphitehq.com>
2021-07-20 22:47:08 -0700

    use f string

--- simple_html/attributes.py
+++ simple_html/attributes.py
@@ -25,7 +25,7 @@ def str_attr(name: str) -> Callable[[str], Attribute]:
 
 
 def style(props: List[Tuple[str, str]]) -> Attribute:
-    return "style", " ".join(["{}: {};".format(key, val) for key, val in props])
+    return "style", " ".join([f"{key}: {val};" for key, val in props])
 
 
 alt = str_attr("alt")
