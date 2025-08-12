23adaf60e321d03ee3e354c32e88692cc577047d
Keith Philpott <keith.philpott@graphitehq.com>
2025-08-12 14:38:08 -0700

    wip

--- tests/test_simple_html.py
+++ tests/test_simple_html.py
@@ -19,10 +19,10 @@ from simple_html import (
     Node,
     DOCTYPE_HTML5,
     render,
-    escape_attribute_key,
     render_styles,
     img,
 )
+from simple_html.utils import escape_attribute_key
 
 
 def test_renders_no_children() -> None:
