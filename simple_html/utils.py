90cd845acc7528a90bf7834d856e48962f28553c
Keith Philpott <keith.philpott@graphitehq.com>
2025-08-14 21:03:54 -0700

    wip

--- simple_html/utils.py
+++ simple_html/utils.py
@@ -1,5 +1,5 @@
 from types import GeneratorType
-from typing import Any, Union, Generator, Iterable, Callable, Final, final
+from typing import Any, Union, Generator, Iterable, Callable, Final
 
 
 class SafeString:
