ab3458af393943d21cb03b08a3ed433864fd1c9d
Keith Philpott <keith.philpott@graphitehq.com>
2025-08-15 15:05:30 -0700

    wip

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 from mypyc.build import mypycify
 
+ext_modules = mypycify([
+    "simple_html/utils.py",
+], opt_level="3")
 
+print("HHHHEEEEUUUU!")
 setup(
     name="simple_html",
-    ext_modules=mypycify([
-        "simple_html/utils.py",
-    ]),
+    ext_modules=ext_modules,
     packages="simple_html",
     python_requires=">=3.9",
 )
