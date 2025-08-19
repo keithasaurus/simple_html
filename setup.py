ecc5511325ef88839d2cb246539c285068238e7a
keithasaurus <592217+keithasaurus@users.noreply.github.com>
2025-08-19 16:48:02 -0700

    tweak release setup (#27)
    * tweak release setup
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * project
    
    * wip
    
    * wip
    
    * wip
    
    * poetry update
    
    * release_setup
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
+try:
+    import tomllib
+except ModuleNotFoundError:
+    # python 3.10 and earlier
+    import tomli as tomllib
+
+from pathlib import Path
 from setuptools import setup
 from mypyc.build import mypycify
 
-ext_modules = mypycify([
-    "simple_html/utils.py",
-])
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open(this_directory / "pyproject.toml", "rb") as f:
+    toml_data = tomllib.load(f)
+
+project_data = toml_data["project"]
 
 setup(
-    name="simple_html",
-    ext_modules=ext_modules,
+    name="simple-html",
+    ext_modules=mypycify([
+        "simple_html/utils.py",
+    ]),
+    author=project_data["authors"][0]["name"],
     packages=["simple_html"],
-    python_requires=">=3.9",
+    python_requires=project_data["requires-python"],
+    version=project_data["version"],
+    description=project_data["description"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license ="MIT",
+    url=project_data["urls"]["Homepage"],
+    classifiers = project_data["classifiers"],
+    keywords=["html", "type hints"]
 )
