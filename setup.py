6358b2ab7acc45ebec1166d84eb5be49ad35a428
Keith Philpott <keith.philpott@graphitehq.com>
2025-08-19 15:21:56 -0700

    wip

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
+import tomllib
 from pathlib import Path
 
 from setuptools import setup
 from mypyc.build import mypycify
 
-ext_modules = mypycify([
-    "simple_html/utils.py",
-])
-
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-
+with open(this_directory / "pyproject.toml", "rb") as f:
+    pyproj_data = tomllib.load(f)
 
 setup(
     name="simple-html",
-    ext_modules=ext_modules,
+    ext_modules=mypycify([
+        "simple_html/utils.py",
+    ]),
     author="Keith Philpott",
     packages=["simple_html"],
     python_requires=">=3.9",
-    version="4.1.2",
+    version=pyproj_data["version"],
     description="Template-less HTML rendering in Python",
     long_description=long_description,
     long_description_content_type='text/markdown',
