2f682566c7f492babf44a6f6b67685cb9f97747b
Keith Philpott <keith.philpott@graphitehq.com>
2025-08-19 15:31:48 -0700

    wip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 with open(this_directory / "pyproject.toml", "rb") as f:
-    pyproj_data = tomllib.load(f)
+    project_data = tomllib.load(f)["project"]
 
 setup(
     name="simple-html",
@@ -18,25 +18,12 @@ setup(
     author="Keith Philpott",
     packages=["simple_html"],
     python_requires=">=3.9",
-    version=pyproj_data["project"]["version"],
-    description="Template-less HTML rendering in Python",
+    version=project_data["version"],
+    description=project_data["description"],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="https://github.com/keithasaurus/simple_html",
-    license = "MIT",
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: MacOS X',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Unix',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        'Typing :: Typed'
-    ],
+    license = project_data["license"],
+    url=project_data["homepage"],
+    classifiers = project_data["classifiers"],
     keywords=["html", "type hints"]
 )
