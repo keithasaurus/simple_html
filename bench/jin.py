3054bedb2d82fda77d9945b18963f7a75e0a8d58
Keith Philpott <keith.philpott@graphitehq.com>
2025-08-28 19:50:52 -0700

    speed up jinja render

--- bench/jin.py
+++ bench/jin.py
@@ -9,28 +9,29 @@ sys.path.append(str(Path(__file__).parent))
 env = Environment(loader=PackageLoader("jinja_example"), autoescape=select_autoescape())
 
 
+_hello_render = env.get_template("hello_world.html").render
 def hello_world_empty(objs: List[None]) -> None:
     for _ in objs:
-        env.get_template("hello_world.html").render()
+        _hello_render()
 
-
+_basic_render = env.get_template("basic.html").render
 def basic(objs: List[Tuple[str, str, List[str]]]) -> None:
     for title, content, oks in objs:
-        env.get_template("basic.html").render(title=title, content=content, oks=oks)
+        _basic_render(title=title, content=content, oks=oks)
 
 
+_basic_long_render = env.get_template("basic_long.html").render
 def basic_long(objs: List[Tuple[str, str, List[str]]]) -> None:
     for title, content, oks in objs:
-        env.get_template("basic_long.html").render(
-            title=title, content=content, oks=oks
-        )
+        _basic_long_render(title=title, content=content, oks=oks)
 
-
+_lorem_render = env.get_template("lorem_ipsum.html").render
 def lorem_ipsum(titles: List[str]) -> None:
     for t in titles:
-        env.get_template("lorem_ipsum.html").render(title=t)
+        _lorem_render(title=t)
 
 
+_render_large_page = env.get_template("large_page.html").render
 def large_page(titles: List[str]) -> None:
     for t in titles:
-        env.get_template("large_page.html").render(title=t)
+        _render_large_page(title=t)
